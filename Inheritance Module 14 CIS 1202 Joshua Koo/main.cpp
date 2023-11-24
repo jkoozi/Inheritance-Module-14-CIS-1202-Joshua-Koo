32abd2213543ad299e210b2137d6a1a8e935a31b
jkoozi <jckoo8282@gmail.com>
2023-11-24 17:44:57 -0500

    Add project files.

---
+++ Inheritance Module 14 CIS 1202 Joshua Koo/main.cpp
@@ -0,0 +1,20 @@
+// Joshua Koo
+// This program uses classes and inheritance to receive and output information about vehicles
+// CIS 1202
+// 11/24/2023
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//Vehicle
+	string manufacturer;
+	int yearBuilt;
+
+	cout << "Enter Vehicle Information:" << std::endl;
+	cout << "Manufacturer: ";
+	cin >> manufacturer;
+	cout << "Year Built: ";
+	cin >> yearBuilt;
+
+}
